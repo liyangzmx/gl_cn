1585d8534b98d30e53245ae8178d9eee34fe5bd7
Li Yang <liyang42@zuoyebang.com>
2021-10-24 02:18:18 +0800

    multi boxes

--- 03_Hello_Window/main.cpp
+++ 03_Hello_Window/main.cpp
@@ -174,6 +174,19 @@ int main(int argc, const char **arg){
     // stbi_image_free(data);
     glBindTexture(GL_TEXTURE_2D, 0);
 
+    glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f), 
+        glm::vec3( 2.0f,  5.0f, -15.0f), 
+        glm::vec3(-1.5f, -2.2f, -2.5f),  
+        glm::vec3(-3.8f, -2.0f, -12.3f),  
+        glm::vec3( 2.4f, -0.4f, -3.5f),  
+        glm::vec3(-1.7f,  3.0f, -7.5f),  
+        glm::vec3( 1.3f, -2.0f, -2.5f),  
+        glm::vec3( 1.5f,  2.0f, -2.5f), 
+        glm::vec3( 1.5f,  0.2f, -1.5f), 
+        glm::vec3(-1.3f,  1.0f, -1.5f)  
+    };
+
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
 
@@ -190,10 +203,6 @@ int main(int argc, const char **arg){
         glBindTexture(GL_TEXTURE_2D, textures[1]);
         glUniform1i(glGetUniformLocation(ourShader.ID, "ourTexture2"), 1);
 
-
-        glm::mat4 model(1.0f);
-        model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 1.0f, 0.0f));
-
         glm::mat4 view(1.0f);
         // 注意，我们将矩阵向我们要进行移动场景的反向移动。
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
@@ -202,16 +211,22 @@ int main(int argc, const char **arg){
         projection = glm::perspective(45.0f, (GLfloat)WIDTH / HEIGHT, 0.1f, 100.0f);
 
         GLint modelLoc = glGetUniformLocation(ourShader.ID, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         GLint viewLoc = glGetUniformLocation(ourShader.ID, "view");
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         GLint projectLoc = glGetUniformLocation(ourShader.ID, "projection");
         glUniformMatrix4fv(projectLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
         glBindVertexArray(VAO);
-        // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO[1]);
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for(GLuint i = 0; i < 10; i++) {
+            glm::mat4 model(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            GLfloat angle = 20.0f * i; 
+            model = glm::rotate(model, angle, glm::vec3(1.0f, 0.3f, 0.5f));
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
+        glBindVertexArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
