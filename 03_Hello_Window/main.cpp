cc350ce40a32541da61fbf574a324011e90610b9
Li Yang <liyang01@syberos.com>
2021-10-23 15:31:47 +0800

    add render command

--- 03_Hello_Window/main.cpp
+++ 03_Hello_Window/main.cpp
@@ -52,7 +52,11 @@ int main(int argc, const char **arg){
 
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
-        
+
+        // Render commands
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
