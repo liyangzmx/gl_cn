563f28e1ff6c6de546157fd6e7974e96ea4e0167
Li Yang <liyang01@syberos.com>
2021-10-23 16:58:11 +0800

    change color by uniform variable

--- 03_Hello_Window/main.cpp
+++ 03_Hello_Window/main.cpp
@@ -53,11 +53,9 @@ int main(int argc, const char **arg){
     // Create Shader Program
     const char *vertexShaderSource = "#version 330 core\n"
         "layout (location = 0) in vec3 aPos;\n"
-        "out vec4 vertexColor;\n"
         "void main()\n"
         "{\n"
         "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0); // 把输出变量设置为暗红色\n"
         "}\n\0";
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -82,10 +80,10 @@ int main(int argc, const char **arg){
     }
     const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
-    "in vec4 vertexColor;\n"
+    "uniform vec4 ourColor;\n"
     "void main()\n"
     "{\n"
-    "   FragColor = vertexColor;\n"
+    "   FragColor = ourColor;\n"
     "}\n\0";
     unsigned int fragmentShader;
     fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
@@ -154,14 +152,19 @@ int main(int argc, const char **arg){
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
+    int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");\
     while(!glfwWindowShouldClose(window)) {
         processInput(window);
+
+        float timeValue = glfwGetTime();
+        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
 
         // Render commands
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
         glBindVertexArray(VAO);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, VBO[1]);
         // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
